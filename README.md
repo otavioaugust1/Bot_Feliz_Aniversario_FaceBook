6dfe1274c16efbb3c6b1edc29bfe9524fa41e778
Otavio Augusto <otavioaugust@gmail.com>
2023-07-03 14:03:19 -0300

    version  0.2

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Bot_Feliz_Aniversario_FaceBook
- 
+# Bot Feliz Aniversário - Facebook
+
+Este é um script em Python para postar mensagens de feliz aniversário na sua própria linha do tempo do Facebook. O script verifica os amigos que estão aniversariando no dia atual e seleciona uma mensagem aleatória para enviar a cada um deles.
+
+## Pré-requisitos
+
+- Python 3.x
+- Biblioteca `facebook-sdk` instalada. Você pode instalá-la usando o seguinte comando:
+
+````pip install facebook-sdk  ````
+
+
+
+## Configuração
+
+1. Obtenha um token de acesso do Facebook para autenticação. Você pode criar um aplicativo no [Facebook Developers](https://developers.facebook.com) e gerar um token de acesso com as permissões necessárias.
+2. Substitua `'YOUR_ACCESS_TOKEN'` pelo seu próprio token de acesso no arquivo `bot_feliz.py`.
+
+## Uso
+
+1. Execute o script `bot_feliz.py` no seu terminal ou IDE Python.
+2. O script irá verificar os amigos aniversariantes no dia atual e postar uma mensagem de feliz aniversário na sua própria linha do tempo do Facebook.
+
+## Customização
+
+- Você pode adicionar suas próprias mensagens de feliz aniversário no script. Basta adicionar as mensagens desejadas na lista `frases` do arquivo `bot_feliz.py`.
+- Se desejar postar em uma página que você administra, em vez da sua própria linha do tempo, você precisará obter um token de acesso com as permissões adequadas para a página e modificar a função `postar_mensagem_aniversario` no arquivo `bot_feliz.py` para usar a ID da página em vez de `"me"`.
+
+## Limitações
+
+Lembre-se de que a API do Facebook não permite mais postagens automáticas em nome dos usuários em suas linhas do tempo. Portanto, o script postará as mensagens na sua própria linha do tempo.
+
+## Contribuições
+
+Contribuições são bem-vindas! Se você encontrar problemas, bugs ou tiver sugestões de melhorias, fique à vontade para abrir uma issue ou enviar um pull request.
+
+## Licença
+
+Este projeto está licenciado sob a [Licença MIT](LICENSE).
+
---
+++ bot_feliz.py
@@ -0,0 +1,87 @@
+# Importação das bibliotecas necessárias
+import random
+import facebook
+import random
+import datetime
+
+# Token de acesso para autenticação na API do Facebook
+access_token = '' # Token de acesso
+
+# Inicialização do objeto Graph da API do Facebook
+graph = facebook.GraphAPI(access_token)
+
+# Mensagens para desejar Feliz Aniversário
+frases = [
+'Desejo que este ano seja ainda mais especial e que realizações continuem acontecendo. Feliz aniversário!',
+'Desejo que este ano seja incrível e que venha muitas oportunidades e alegrias. 🎉🎂Feliz aniversário!',
+'Desejo que este ano seja incrível e que venha muitas realizações. Feliz aniversário!',
+'Desejo que este ano seja incrível e que venha muitas surpresas boas. Feliz aniversário!',
+'Desejo que este ano seja maravilhoso e que venha muitas oportunidades e alegrias.🎉🎂 Feliz aniversário!',
+'Desejo que este ano seja maravilhoso e que venha muitas oportunidades incríveis. Feliz aniversário!',
+'Desejo que este ano seja maravilhoso e que venha muitas realizações e alegrias. Feliz aniversário!',
+'Desejo que este ano seja repleto de amor, paz e realizações incríveis. Feliz aniversário!',
+'Desejo que este ano seja repleto de amor, paz e realizações. Feliz aniversário!',
+'Desejo que este ano traga muita alegria, amor e realizações incríveis. Feliz aniversário!🎉🎂',
+'Desejo que este ano traga muita alegria, sucesso e realizações incríveis. Feliz aniversário!',
+'Desejo que este ano traga muita paz, amor e felicidade. Feliz aniversário!',
+'Desejo que este ano traga muito amor, paz e realizações incríveis. Feliz aniversário!',
+'Desejo que este ano traga muito sucesso, amor e realizações incríveis.🎉🎂 Feliz aniversário!',
+'Parabéns pelo aniversário! Que este novo ano traga muita alegria e realizações incríveis.',
+'Parabéns pelo aniversário! Que este novo ano traga muita alegria e realizações.',
+'Parabéns pelo aniversário!🎉🎂 Que este novo ano traga muita realização e alegria.',
+'Parabéns pelo aniversário! Que o dia seja especial e repleto de alegria.',
+'Parabéns pelo aniversário! Que venha muitas alegrias e oportunidades incríveis.',
+'Parabéns pelo aniversário! 🎉🎂Que venha muitas alegrias e realizações.',
+'Parabéns pelo aniversário! Que venha muitas aventuras e conquistas incríveis.',
+'Parabéns pelo aniversário! Que venha muitas aventuras e felicidade.',
+'Parabéns pelo aniversário! Que venha muitas aventuras e momentos inesquecíveis.',
+'Parabéns pelo aniversário! Que venha muitas aventuras e que os dias sejam repletos de felicidade.',
+'Parabéns pelo aniversário! Que venha muitas aventuras e realizações incríveis.',
+'Parabéns pelo aniversário! Que venha muitas aventuras e realizações.',
+'Que este ano seja maravilhoso e que todos os teus sonhos se tornem realidade. Feliz aniversário!',
+'Que este ano seja maravilhoso e que venha muitas realizações e alegrias. Feliz aniversário!',
+'Que este ano seja maravilhoso e que venha muitas realizações. Feliz aniversário!',
+'Que este ano seja maravilhoso e que venha muitas surpresas boas e realizações. Feliz aniversário!',
+'Que este ano seja repleto de realizações e momentos inesquecíveis. Feliz aniversário!',
+'Que este ano traga muitas alegrias e realizações incríveis. Feliz aniversário!',
+'Que este ano trazida muitas surpresas boas e conquistas incríveis. Feliz aniversário!',
+'Que venha muitas alegrias e realizações neste novo ano de vida. Feliz aniversário!',
+'Que venha muitas surpresas boas e realizações neste novo ano de vida. Feliz aniversário!',
+"Feliz aniversário! Hoje você completa mais um ano de vida e é hora de comemorar com muita alegria. Que seu dia seja repleto de luz e paz. Que as pessoas queridas estejam com você e que o amor invada seu coração!",
+"Que seu aniversário seja repleto de palavras de carinho e abraços sinceros. Parabéns! ❤️😍🎉",
+"Feliz Aniversário! 🎉🎂 Mais um ano que passou e outro que vai começar. Aproveite ao máximo e que nunca lhe falte felicidade, amor, saúde e amizade. 🤗",
+"Feliz aniversário! Que tudo de bom lhe aconteça neste dia tão marcante e especial na sua vida. Aproveite com um grande sorriso no rosto, e divirta-se muito!",
+"Completar mais um ano neste mundo é o maior presente que podemos desejar. Feliz aniversário e que a vida lhe sorria sempre!",
+"Parabéns! Que o seu dia seja tão lindo quanto o seu sorriso e lhe ofereça tanta felicidade quanto a sua amizade envia para a minha vida. Que esta nova etapa chegue recheada de muita saúde e novas oportunidades para concretizar os seus sonhos mais desejados.",
+"Hoje você inicia uma nova jornada, e nesse momento de alegria por você estar completando mais um ano de vida, quero lhe dizer que tenho muito orgulho em compartilhar da sua amizade. Parabéns meu amigo, e feliz aniversário!",
+"Parabéns por esta data especial. Que o tempo seja sempre o seu melhor parceiro, trazendo serenidade, equilíbrio e sabedoria – que lhe darão a receita ideal de como viver a vida, aproveitando o melhor que ela tem a oferecer.",
+"Nesta data especial em que você comemora mais um ano de vida, eu quero lhe desejar um dia muito feliz e preenchido por doces surpresas e presentes. Que possa celebrar o seu dia em grande estilo e na companhia daqueles que mais ama. Parabéns!",
+"Que este aniversário traga muita paz, saúde, alegria e sucesso. O pouco que conheço de você, já me diz que você é uma pessoa muito especial e que sab aproveitar o lado bom da vida.",
+"Feliz Aniversário! Este é um dia grandioso, pois o milagre da vida se renova para você em mais um ciclo que começa hoje.",
+"Parabéns, e muitos anos de vida! Que o dia seja generoso e cheio de alegria, paz e energia positiva."
+]
+
+# Função para obter os amigos aniversariantes no dia atual
+def obter_amigos_aniversariantes():
+    today = datetime.date.today().strftime('%m-%d')
+    amigos = graph.get_connections("me", "friends", fields="birthday")
+    aniversariantes = []
+    for amigo in amigos["data"]:
+        if "birthday" in amigo and amigo["birthday"] == today:
+            aniversariantes.append(amigo)
+    return aniversariantes
+
+# Função para postar mensagem de feliz aniversário na linha do tempo
+def postar_mensagem_aniversario(mensagem):
+    graph.put_object("me", "feed", message=mensagem)
+
+# Obtendo a lista de amigos aniversariantes
+amigos_aniversariantes = obter_amigos_aniversariantes()
+
+# Percorrendo a lista de amigos aniversariantes e postando as mensagens de feliz aniversário
+for amigo in amigos_aniversariantes:
+    amigo_nome = amigo["name"]
+    frase_sorteada = random.choice(frases)
+    mensagem = f"Parabéns, {amigo_nome}! {frase_sorteada}"
+    postar_mensagem_aniversario(mensagem)
+    print(f"Mensagem enviada para {amigo_nome} na sua linha do tempo.")
